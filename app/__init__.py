7190e57cc422902b3289cae5e632ede2a581d64d
lDuReyl <ldureyl@mail.ru>
2023-05-06 18:03:47 +0300

    2

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
 db = SQLAlchemy(app)
 
